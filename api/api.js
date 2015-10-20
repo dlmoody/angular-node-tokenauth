0b1561975f9650ec46dc11d295b5b69b6bffcb19
David Moody <moody.david@gmail.com>
2015-10-20 16:10:16 -0400

    Added jwt encoding in the jwt service

--- api/api.js
+++ api/api.js
@@ -2,6 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var User = require('./model/User');
+var jwt = require('./services/jwt');
+
 
 
 var app = express();
@@ -25,13 +27,26 @@ app.post('/register', function (req, res) {
     password: user.password
   });
 
+  var payload = {
+    iss: req.hostname,
+    sub: user._id
+  };
+
+  var token = jwt.encode(payload, "shhh...");
+
+
+
   newUser.save(function (err) {
-    res.status(200).send(newUser.toJSON());
+    res.status(200).send({
+      user: newUser.toJSON(),
+      token: token
+    });
   });
 
 });
 
 mongoose.connect('mongodb://localhost/psjwt');
+
 
 var server = app.listen(3000, function () {
   console.log('api listening on ', server.address().port);
